671dd3f55ed034294bcada18b93fc0a62365faa7
B36559 <esoto@intercorp.com.pe>
2026-09-11 21:36:36 -0500

    fix: agregar @Repository a OAuthStateRepository

--- Arquitectura-Clean/src/main/java/com/andinaseguros/interfaceadapters/out/persistence/mongodb/OAuthStateRepository.java
+++ Arquitectura-Clean/src/main/java/com/andinaseguros/interfaceadapters/out/persistence/mongodb/OAuthStateRepository.java
@@ -1,5 +1,7 @@
 package com.andinaseguros.interfaceadapters.out.persistence.mongodb;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OAuthStateRepository extends MongoRepository<OAuthStateDocument, String> {}
